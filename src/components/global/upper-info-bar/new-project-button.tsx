609d7a4e5ee3c68beab02fc6197a737a0f8a0d23
Aryanpatel2001 <patelaryan8044@gmail.com>
2025-03-11 14:36:34 +0530

    create upper info bar serch,toggle,import,next project,theme

---
+++ src/components/global/upper-info-bar/new-project-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { User } from "@prisma/client";
+import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const NewProjectButton = ({ user }: { user: User }) => {
+  const router = useRouter();
+
+  return (
+    <Button
+      className="rounded-lg font-semibold text-black bg-gray-300"
+      disabled={!user.subscription}
+    >
+      <Plus />
+      New Project
+    </Button>
+  );
+};
+
+export default NewProjectButton;
