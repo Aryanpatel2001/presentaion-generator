493bba9218a80e5a58fbbbc39937fa670d142986
Aryanpatel2001 <patelaryan8044@gmail.com>
2025-06-13 18:34:21 +0530

    Add Subscription and payment feature

---
+++ src/app/api/webhook/subscription/route.ts
@@ -0,0 +1,55 @@
+export const dynamic = "force-dynamic";
+import crypto from "node:crypto";
+import { NextRequest } from "next/server";
+import { client } from "@/lib/prisma";
+
+export async function POST(req: NextRequest) {
+  try {
+    const rawBody = await req.text();
+    const body = JSON.parse(rawBody);
+
+    const { buyerUserId } = body.meta.custom_data;
+
+    if (!buyerUserId) {
+      throw new Error("Invalid buyerUserId or id does not exist");
+    }
+
+    const hmac = crypto.createHmac(
+      "sha256",
+      process.env.LEMONSQUEEZY_WEBHOOK_SECRET!
+    );
+
+    const digest = Buffer.from(hmac.update(rawBody).digest("hex"), "utf-8");
+
+    const signature = Buffer.from(
+      req.headers.get("X-Signature") || "",
+      "utf-8"
+    );
+
+    console.log(`digest:`, digest);
+    console.log(`signature:`, signature);
+
+    if (!crypto.timingSafeEqual(digest, signature)) {
+      throw new Error("Invalid signature");
+    }
+
+    const buyer = await client.user.update({
+      where: {
+        id: buyerUserId,
+      },
+      data: {
+        subscription: true,
+      },
+    });
+    if (!buyer) {
+      return Response.json({
+        message: "Cannot update the subscription.",
+        status: 404,
+      });
+    }
+    return Response.json({ data: buyer, status: 200 });
+  } catch (error) {
+    console.error("Error in POST handler:", error);
+    return Response.json({ message: "Internal server Error", status: 500 });
+  }
+}
